120b304ebf1e9d66138a1af35cd064cd9cfe14f1
Mys <jp6smys@gmail.com>
2024-12-05 15:09:28 +0900

    コメントを追加

--- src/sample/Calculator.java
+++ src/sample/Calculator.java
@@ -1,11 +1,26 @@
 package sample;
 
+/**
+ * 計算クラス
+ */
 public class Calculator {
 	
+    /**
+     * 加算処理
+     * @param a
+     * @param b
+     * @return 加算結果
+     */
     public int add(int a, int b) {
         return a + b;
     }
     
+    /**
+     * 減算処理
+     * @param a
+     * @param b
+     * @return　減算結果
+     */
     public int subtract(int a, int b) {
         return a - b;
     }
